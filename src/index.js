c2061a740b67141685b8679a3639427d93563a9f
Statz Tatsumi Saines <statsumi.sts@gmail.com>
2023-11-07 16:30:57 -0500

    statz's code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
-const form = document.getElementById("movie-list")
+//grab movie-list nav element
+const movieList = document.getElementById("movie-list")
+//get all the movies
+fetch('http://localhost:3000/movies')           //api for the get request
+  .then(response => response.json())
+  .then(movies => {
+    const image = document.createElement('img');
+    const iconurl = 'http://localhost:3000/movies'
+    movies.forEach(element => document.createElement('img'));
+    image.setAttribute('src', iconurl);
+    li.appendChild(image)
 
+    //for each movie 
+
+        //create an image
+        //append to movie-list nav element
+  })
 
 form.addEventListener("Submit", (e) => {
     e.preventDefault()
     
 })
+
+var image = document.createElement("img");
+image.setAttribute("src", iconurl);
+li.appendChild(image);
